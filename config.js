8c0f1a7b5479f5d2d68ee0809d104f07870b7b28
rpshende <rpshende@DELL-PRECISION5000>
2022-01-30 12:36:40 -0500

    reformat files

--- config.js
+++ config.js
@@ -2,8 +2,15 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDG7INvh1D20lIMQ0HUhIOHZy15H5WTXAc",
+    authDomain: "team-voting-app-332b3.firebaseapp.com",
+    databaseURL: "https://team-voting-app-332b3-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-332b3",
+    storageBucket: "team-voting-app-332b3.appspot.com",
+    messagingSenderId: "832700178591",
+    appId: "1:832700178591:web:3e57fc28f133d0a5dd42aa"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
